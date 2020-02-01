3e8899b5ea1d5466adbf20ab29f1429abfb3b564
Matt Haywood <matthaywood2012@gmail.com>
2020-02-01 14:39:11 +0000

    added procfile

--- app.js
+++ app.js
@@ -8,7 +8,30 @@ var multer = require("multer");
 var LocalStrategy = require("passport-local");
 var passport = require("passport");
 var passportLocalMongoose = require("passport-local-mongoose");
-var middleware = require("./middleware")
+var middleware = require("./middleware");
+var axios = require('axios');
+var cheerio = require('cheerio');
+
+//TENANT SCHEMA
+var TenantSchema = new mongoose.Schema({
+    firstname: String,
+    lastname: String,
+    email: String,
+    mobile_number: String,
+    home_number: String,
+    animals: Number,
+    property: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Property"
+    },
+    CurrentTenant: Boolean,
+    date_moved_in: String,
+    date_moved_out: String
+})
+
+var Tenant = mongoose.model("Tenant", TenantSchema)
+
+
 
 //PROPERTY SCHEMA
 var propertySchema = new mongoose.Schema({
@@ -24,7 +47,23 @@ var propertySchema = new mongoose.Schema({
         },
         username: String
     },
-    first: String
+    first: String,
+    estimatedValue: [{
+        year: String,
+        month: String,
+        price: Number
+    }],
+    zoopla_id: Number,
+    maintenance_item: [{
+        Name: String,
+        Detail: String,
+        Finish_Date: String,
+        Done: Boolean
+    }],
+    Tenants: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tenant"
+    }]
 
 });
 
@@ -171,6 +210,7 @@ function createNewProperty(req, res, next) {
 
 
 
+
     console.log(name);
 
     var newProperty = {
@@ -182,7 +222,7 @@ function createNewProperty(req, res, next) {
         author: {
             id: currentUser,
             username: currentUser.username
-        }
+        },
     }
 
     Property.create(newProperty, function (err, _property) {
@@ -402,8 +442,12 @@ app.get("/user/:id/manage/:propid", function (req, res) {
     var userid = req.params.id
     var propid = req.params.propid;
     var images = []
+    var currentPrice;
+    var url = 'https://www.zoopla.co.uk/property/';
 
-    Property.findById(propid).exec(function (err, foundProperty) {
+
+
+    Property.findById(propid).populate("Tenants").exec(function (err, foundProperty) {
         if (err) {
             console.log(err);
         } else {
@@ -417,11 +461,176 @@ app.get("/user/:id/manage/:propid", function (req, res) {
                         images.push(file);
                     }
                 }
-                res.render("manageproperty.ejs", { property: foundProperty, images: images });
-        })
-    }
-    
+
+
+                var d = new Date();
+                var month = new Array();
+                month[0] = "January";
+                month[1] = "February";
+                month[2] = "March";
+                month[3] = "April";
+                month[4] = "May";
+                month[5] = "June";
+                month[6] = "July";
+                month[7] = "August";
+                month[8] = "September";
+                month[9] = "October";
+                month[10] = "November";
+                month[11] = "December";
+                var themonth = month[d.getMonth()];
+                console.log(foundProperty)
+                var theyear = d.getFullYear();
+
+                console.log(foundProperty.estimatedValue.length)
+                if (foundProperty.estimatedValue[0] === undefined || foundProperty.estimatedValue === 0 || foundProperty.estimatedValue[foundProperty.estimatedValue.length - 1].month !== themonth) {
+                    url = url + foundProperty.zoopla_id;
+                    console.log(url);
+                    axios(url)
+                        .then(response => {
+                            const html = response.data;
+                            const $ = cheerio.load(html);
+                            const estimatedPrice = $('.pdp-estimate__price');
+                            console.log("Scraped from Zoopla")
+                            currentPrice = estimatedPrice.text();
+                            currentPrice = currentPrice.replace(',', '');
+                            currentPrice = currentPrice.replace(',', '');
+                            currentPrice = currentPrice.split('£');
+                            currentRent = currentPrice[2];
+                            currentPrice = currentPrice[1];
+                            console.log(currentPrice);
+                            
+                            function pushNewprop(callback){
+                                foundProperty.estimatedValue.push({
+                                    year: theyear,
+                                    month: themonth,
+                                    price: currentPrice
+                                })
+                                callback(); 
+                            }
+
+                            pushNewprop(function(err){
+                                Property.findByIdAndUpdate(propid, foundProperty, {new: true}, function (err, updatedProperty) {
+                                    if (err) {
+                                        console.log(err)
+                                    } else {
+                                        console.log("PROPERTY UPDATED!!!!")
+                                        console.log(updatedProperty)
+                                        res.render("manageproperty.ejs", { property: updatedProperty, images: images });
+                                        }
+                                    })
+                            })
+
+                        })
+                        
+                } else {
+                    res.render("manageproperty.ejs", { property: foundProperty, images: images });
+                }
+
+            })
+        }
     })
+})
+
+//Route for updating the estimated value and rent
+
+app.put("/user/:id/manage/:propid", function (req, res) {
+    
+    Property.findByIdAndUpdate(req.params.id, req.body.property, function (err, updatedProperty) {
+        if (err) {
+            console.log("error has occurred");
+        } else {
+            console.log(req.body.property);
+            res.redirect("/properties");
+        }
+    })
+})
+
+//Route for adding a maintenance job to the house
+app.put("/user/:id/properties/:propid/job", function (req,res) {
+    jobname = req.body.jobname;
+    jobbody = req.body.jobbody;
+    jobdate = req.body.jobdate;
+    
+
+    id = req.params.id;
+    Property.findById(req.params.propid, function (err, updatedProperty) {
+        if (err) {
+            console.log("error has occurred");
+        } else {
+            updatedProperty.maintenance_item.push({
+                Name: jobname,
+                Detail: jobbody,
+                Finish_Date: req.body.jobdate
+            })
+            console.log(updatedProperty);
+            Property.findByIdAndUpdate(req.params.propid, updatedProperty, function(err, updatedProperty) {
+                if(err) {
+                    console.log(err);
+                }else{
+                    res.redirect("/user/" + id + "/manage/" + updatedProperty.id);
+                }
+            })
+            
+        }
+    })
+})
+
+//ROUTES FOR TENANTS
+
+//NEW TENANT ROUTE
+
+function createNewTenant(req,res,next){
+
+    var firstname = req.body.firstname;
+    var lastname = req.body.lastname;
+    var email = req.body.email;
+    var mobile_number = req.body.mobile_number;
+    var home_number = req.body.house_number;
+    var animals = req.body.animals;
+    var propid = req.params.id;
+
+
+    var newTenant = {
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        mobile_number: mobile_number,
+        home_number: home_number,
+        animals: animals,
+        property: propid,
+        CurrentTenant: true,
+        
+    }
+
+    Tenant.create(newTenant, function (err, _tenant) {
+        if (err) {
+            console.log(err);
+        } else { }
+        Property.findById(propid, function(err, foundProperty){
+            foundProperty.Tenants.push(_tenant);
+            foundProperty.save(function (err,data){
+                if(err) {
+                    console.log(err);
+                } else {
+                    next();
+                }
+            })
+        })
+    });
+
+    
+}
+
+
+app.post("/properties/:id/new-tenant", createNewTenant, function(req,res){
+    res.render("home.ejs");
+})
+
+app.delete("/properties/:id/new-tenant/:tid", function(req,res){
+    Tenant.findByIdAndRemove(req.params.tid, function(err){
+        res.redirect("/properties");
+    })
+    
 })
 
 
